e6bb59dea5e9c8a31934c0a2226f6a3434bc6f5d
ANDREIA PEREIRA DOS SANTOS <andreiasantos@sabesp.com.br>
2023-01-05 22:47:13 -0300

    Segunda Solução - Aula 2

---
+++ 02-ComandoRepeticao_While/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02_ComandoRepeticao_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 10) {
+                Console.WriteLine($": {i} x 5 = {i * 5}");
+                i++; // i = i+1
+            }
+            Console.WriteLine("------------------------");
+
+            for (int j = 0; j <= 10; j++)
+            {
+                Console.WriteLine($": {j} x 6 = {j * 6}");                
+            }
+
+            {
+
+               
+        }
+        }
+    }
+}
